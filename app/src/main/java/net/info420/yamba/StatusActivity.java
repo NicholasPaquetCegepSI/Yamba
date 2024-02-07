43604a6aeaed0ac3453d22b8455a6f65763b8cb1
Nicholas Paquet <nicholas.paquet.rowsell@cegepsi.ca>
2024-02-07 15:46:18 -0500

    Creation du layout

--- app/src/main/java/net/info420/yamba/StatusActivity.java
+++ app/src/main/java/net/info420/yamba/StatusActivity.java
@@ -4,13 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class StatusActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = "StatusActivity";
+    Button buttonUpdate;
+    EditText editStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.status_layout);
+
+        editStatus = findViewById(R.id.editStatus);
+        buttonUpdate = findViewById(R.id.buttonUpdate);
+
+        buttonUpdate.setOnClickListener(this);
     }
 
     @Override
