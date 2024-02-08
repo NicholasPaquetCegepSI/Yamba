83895b5e7f2954f9ff708da865d6e8a1aeaa2ca7
Nicholas Paquet <nicholas.paquet.rowsell@cegepsi.ca>
2024-02-08 13:30:07 -0500

    Lancement d'un toast lors de l'envoi d'un toot

--- app/src/main/java/net/info420/yamba/StatusActivity.java
+++ app/src/main/java/net/info420/yamba/StatusActivity.java
@@ -3,10 +3,14 @@ package net.info420.yamba;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import social.bigbone.MastodonClient;
 import social.bigbone.MastodonRequest;
@@ -19,6 +23,8 @@ public class StatusActivity extends AppCompatActivity implements View.OnClickLis
     Button buttonUpdate;
     MastodonClient client;
     MastodonRequest<Status> request;
+    Handler handler;
+    final String[] message = new String[1];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +36,7 @@ public class StatusActivity extends AppCompatActivity implements View.OnClickLis
         buttonUpdate = findViewById(R.id.buttonUpdate);
         buttonUpdate.setOnClickListener(this);
 
-
+        handler = new Handler();
     }
 
     @Override
@@ -44,10 +50,14 @@ public class StatusActivity extends AppCompatActivity implements View.OnClickLis
                 client = new MastodonClient.Builder(getString(R.string.instanceHostname)).accessToken(getString(R.string.accessToken)).build();
                 request = client.statuses().postStatus(status);
                 request.execute();
+
                 Log.d(TAG, "onClick(): " + getString(R.string.tootIsSent) + " : " + status);
+                message[0] = getString(R.string.tootIsSent);
             } catch (BigBoneRequestException e) {
                 Log.d(TAG, "onClick(): " + getString(R.string.bigBoneException) + " : " + status);
+                message[0] = getString(R.string.bigBoneException);
             }
+            handler.post(() -> Toast.makeText(StatusActivity.this, message[0], Toast.LENGTH_LONG).show());
         }).start();
     }
 }
