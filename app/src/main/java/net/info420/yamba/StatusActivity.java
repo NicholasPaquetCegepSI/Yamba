ef3f390fa88f5762a3a28b6777722547fa7534d8
Nicholas Paquet <nicholas.paquet.rowsell@cegepsi.ca>
2024-02-12 16:10:59 -0500

    Création d'un service de récupération de toot qui roule aux 30 secondes.

--- app/src/main/java/net/info420/yamba/StatusActivity.java
+++ app/src/main/java/net/info420/yamba/StatusActivity.java
@@ -2,6 +2,8 @@ package net.info420.yamba;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
@@ -9,8 +11,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
-import java.util.concurrent.atomic.AtomicBoolean;
 
 import social.bigbone.MastodonClient;
 import social.bigbone.MastodonRequest;
@@ -20,7 +20,8 @@ import social.bigbone.api.exception.BigBoneRequestException;
 public class StatusActivity extends AppCompatActivity implements View.OnClickListener {
     private static final String TAG = "StatusActivity";
     EditText editStatus;
-    Button buttonUpdate;
+    Button buttonUpdate, buttonStartUpdaterService, buttonStopUpdaterService;
+    Intent intentUpdaterService;
     MastodonClient client;
     MastodonRequest<Status> request;
     Handler handler;
@@ -34,30 +35,58 @@ public class StatusActivity extends AppCompatActivity implements View.OnClickLis
         // Composantes
         editStatus = findViewById(R.id.editStatus);
         buttonUpdate = findViewById(R.id.buttonUpdate);
+        buttonStartUpdaterService = findViewById(R.id.buttonStartUpdaterService);
+        buttonStopUpdaterService = findViewById(R.id.buttonStopUpdaterService);
+
+        intentUpdaterService = new Intent(this, UpdaterService.class);
+
+        // Listeners
         buttonUpdate.setOnClickListener(this);
+        buttonStartUpdaterService.setOnClickListener(this);
+        buttonStopUpdaterService.setOnClickListener(this);
 
         handler = new Handler();
     }
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     public void onClick(View view) {
         String status;
 
-        status = editStatus.getText().toString();
+        switch (view.getId()) {
+            case R.id.buttonUpdate:
+                status = editStatus.getText().toString();
 
-        new Thread(() -> {
-            try {
-                client = new MastodonClient.Builder(getString(R.string.instanceHostname)).accessToken(getString(R.string.accessToken)).build();
-                request = client.statuses().postStatus(status);
-                request.execute();
+                new Thread(() -> {
+                    try {
+                        client = new MastodonClient.Builder(getString(R.string.instanceHostname)).accessToken(getString(R.string.accessToken)).build();
+                        request = client.statuses().postStatus(status);
+                        request.execute();
 
-                Log.d(TAG, "onClick(): " + getString(R.string.tootIsSent) + " : " + status);
-                message[0] = getString(R.string.tootIsSent);
-            } catch (BigBoneRequestException e) {
-                Log.d(TAG, "onClick(): " + getString(R.string.bigBoneException) + " : " + status);
-                message[0] = getString(R.string.bigBoneException);
-            }
-            handler.post(() -> Toast.makeText(StatusActivity.this, message[0], Toast.LENGTH_LONG).show());
-        }).start();
+                        Log.d(TAG, "onClick(): " + getString(R.string.tootIsSent) + " : " + status);
+                        message[0] = getString(R.string.tootIsSent);
+                    } catch (BigBoneRequestException e) {
+                        Log.d(TAG, "onClick(): " + getString(R.string.bigBoneException) + " : " + status);
+                        message[0] = getString(R.string.bigBoneException);
+                    }
+                    handler.post(() -> Toast.makeText(StatusActivity.this, message[0], Toast.LENGTH_LONG).show());
+                }).start();
+                break;
+
+            case R.id.buttonStartUpdaterService:
+                startService(intentUpdaterService);
+                break;
+
+            case R.id.buttonStopUpdaterService:
+                stopService(intentUpdaterService);
+                break;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        stopService(intentUpdaterService);
     }
 }
---
+++ app/src/main/java/net/info420/yamba/UpdaterService.java
@@ -0,0 +1,176 @@
+package net.info420.yamba;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.IBinder;
+import android.text.format.DateUtils;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import social.bigbone.MastodonClient;
+import social.bigbone.api.Pageable;
+import social.bigbone.api.Range;
+import social.bigbone.api.entity.Status;
+import social.bigbone.api.exception.BigBoneRequestException;
+
+public class UpdaterService extends Service {
+    private static final String TAG = "UpdaterService";
+    boolean updaterServiceDejaDemarre = false;
+    MastodonClient client;
+    Handler handler;
+    final String[] message = new String[1];
+    String id;
+    String dateIOS8601;
+    SimpleDateFormat formatDateISO8601;
+    Date dateJava;
+    long tempsMillisecondes;
+    CharSequence tempsRelatif;
+    String idUsager;
+    String nomUsager;
+    String texteBrut;
+    String texteFinal;
+    private static final long DELAY = 30000;
+    boolean updaterServiceIsRequesting;
+
+    public UpdaterService() {
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Log.d(TAG, "onCreate()");
+        handler = new Handler();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (!updaterServiceDejaDemarre) {
+            updaterServiceDejaDemarre = true;
+            Log.d(TAG, "onStartCommand() : " + getString(R.string.updaterServiceStarted));
+            Toast.makeText(this, R.string.updaterServiceStarted, Toast.LENGTH_LONG).show();
+
+            new Thread(() -> {
+                updaterServiceIsRequesting = true;
+
+                while (updaterServiceIsRequesting) {
+                    try {
+                        client = new MastodonClient.Builder(getString(R.string.instanceHostname)).accessToken(
+                                getString(R.string.accessToken)).build();
+                        // Extraction des 5 derniers toots du timeline de l'usager.
+                        Pageable<Status> timeline = client.timelines().getHomeTimeline(new Range(null, null, null, 5))
+                                .execute();
+                        Log.d(TAG, "Thread run(): " + getString(R.string.timelineReceived));
+                        message[0] = getString(R.string.timelineReceived);
+
+                        // Extraction de diverses données de chaque toot du timeline.
+                        timeline.getPart().forEach(status -> {
+                            try {
+                                // Extraction de l'identifiant du toot.
+                                id = status.getId();
+
+                                // Extraction de la date sous la norme ISO 8601.
+                                // La date extraite de type PrecisionDateTime. Elle est convertie en chaîne.
+                                dateIOS8601 = String.valueOf(status.getCreatedAt());
+                                // Création du profil/modèle de la date extraite sous forme texte, afin de pouvoir la
+                                // convertir en Date Java.
+                                formatDateISO8601 = new SimpleDateFormat(
+                                        "'ExactTime(instant='yyyy-MM-dd'T'HH:mm:ss.SSS'Z')");
+
+                                // Création d'une date Java, à partir de la date extraite sous forme de texte et de son
+                                // profil/modèle.
+                                dateJava = formatDateISO8601.parse(dateIOS8601);
+
+                                // Calcul du nombre de millisecondes écoulées depuis le 1 janvier 1970, à partir de la
+                                // Date Java.
+                                tempsMillisecondes = dateJava.getTime();
+
+                                // Création du temps relatif, à partir du nombre de secondes écoulées depuis le 1 janvier
+                                // 1970.
+                                tempsRelatif = DateUtils.getRelativeTimeSpanString(tempsMillisecondes);
+
+                                // Extraction de l'identifiant de l'usager.
+                                idUsager = status.getAccount().getUsername();
+
+                                // Extraction du nom de l'usager tel qu'affiché dans chaque toot.
+                                nomUsager = status.getAccount().getDisplayName();
+
+                                // Extraction du texte brut du toot (avec les tags HTML)
+                                texteBrut = status.getContent();
+
+                                // Élimination des tags HTML du texte brut du toot.
+                                // La méthode replaceAll() retourne une nouvelle chaîne de caractères dans laquelle
+                                // toutes les occurrences d'un motif donné ont été remplacées par une chaîne de
+                                // remplacement.
+                                // Le 1er argument (description d'un motif) peut être une chaîne de caractères ou une
+                                // expression régulière (regex).
+                                // Le 2e argument (remplacement) peut être une chaîne de caractères ou une fonction qui
+                                // sera appelée pour chaque correspondance.
+                                // La chaîne de caractères initiale reste inchangée.
+                                // < : Correspond au symbole débutant le tag.
+                                // [^>*] : Correspond à tout caractère n'étant pas le symbole >.
+                                // > : Correspond au symbole terminant le tag.
+                                // "" : Remplace les correspondances avec une chaîne vide.
+                                texteFinal = status.getContent().replaceAll("<[^>]*>", "");
+
+                                // Insertion au Logcat des diverses données extraites du toot.
+                                Log.d(TAG, "Id du toot : " + id);
+                                Log.d(TAG, "Date ISO 8601 : " + dateIOS8601);
+                                Log.d(TAG, "Date Java : " + dateJava);
+                                Log.d(TAG, "Temps en millisecondes : " + tempsMillisecondes);
+                                Log.d(TAG, "Temps relatif : " + tempsRelatif);
+                                Log.d(TAG, "Texte avec tags HTML : " + texteBrut);
+                                Log.d(TAG, "Texte sans tags HTML : " + texteFinal);
+                                Log.d(TAG, "Identifiant de l'usager : " + idUsager);
+                                Log.d(TAG, "Nom de l'usager : " + nomUsager);
+
+
+                            } catch (ParseException e) {
+                                Log.d(TAG, "Thread run(): " + getString(R.string.exception), e);
+                                message[0] = getString(R.string.exception);
+                                updaterServiceIsRequesting = false;
+                            }
+
+                        });
+                        Thread.sleep(DELAY);
+                    } catch (BigBoneRequestException e) {
+                        Log.d(TAG, "Thread run(): " + getString(R.string.bigBoneException), e);
+                        message[0] = getString(R.string.bigBoneException);
+                    } catch (InterruptedException e) {
+                        Log.d(TAG, "Thread run(): " + getString(R.string.exception), e);
+                        message[0] = getString(R.string.exception);
+                        updaterServiceIsRequesting = false;
+                    }
+                    handler.post(() -> {
+                        Toast.makeText(UpdaterService.this, message[0], Toast.LENGTH_LONG).show();
+                    });
+                }
+            }).start();
+
+        } else {
+            Log.d(TAG, "onStartCommand() : " + getString(R.string.updaterServiceAlreadyStarted));
+            Toast.makeText(this, R.string.updaterServiceAlreadyStarted, Toast.LENGTH_LONG).show();
+        }
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        Log.d(TAG, "onDestroy() : " + getString(R.string.updaterServiceStopped));
+        Toast.makeText(this, R.string.updaterServiceStopped, Toast.LENGTH_LONG).show();
+        updaterServiceIsRequesting = false;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
